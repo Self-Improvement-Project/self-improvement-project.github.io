364e7d9819dab7c061f48a5bc31da59d7da2430a
Matt Stillwell <stillwell006@gmail.com>
2022-05-06 00:09:31 -0400

    :lipstick: Add twitter and youtube icon buttons

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,4 +1,4 @@
-import { GitHub } from "@mui/icons-material";
+import { GitHub, Twitter, YouTube } from "@mui/icons-material";
 import { Box, Button, Container, Grid, IconButton } from "@mui/material";
 // import BookIcon from '@mui/icons-material/Book';
 import React from 'react';
@@ -109,12 +109,23 @@ const App = () => {
               </Button>
             </Box>
 
-          <IconButton
-            href="https://github.com/Self-Improvement-Project/self-improvement-project.github.io"
-          >
-            <GitHub />
-          </IconButton>
-
+          <Box>
+            <IconButton
+                href="https://twitter.com/lawrence__mcl"
+            >
+              <Twitter />
+            </IconButton>
+            <IconButton
+                href="https://www.youtube.com/channel/UCSENKoMvOBCNtHJF0SP4znA"
+            >
+              <YouTube />
+            </IconButton>
+            <IconButton
+              href="https://github.com/Self-Improvement-Project/self-improvement-project.github.io"
+            >
+              <GitHub />
+            </IconButton>
+          </Box>
         </header>
       </Container>
     </div>
